f7190fc53a181cf2ef5d0bb0a3a030c80ff8e508
Mongezi MAKATSHU <mmakatsh@c3r8s1.wethinkcode.co.za>
2018-06-15 09:12:37 +0200

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakatsh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/08 10:09:42 by mmakatsh          #+#    #+#             */
+/*   Updated: 2018/06/15 08:22:58 by mmakatsh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <unistd.h>
+
+int					get_next_line(const int fd, char **line);
+
+#endif
